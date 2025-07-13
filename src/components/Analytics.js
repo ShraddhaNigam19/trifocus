d8b610ac77ad4566bbd1eed2db64b905bc680484
ShraddhaNigam19 <shraddhanigam.19@gmail.com>
2025-07-13 23:06:33 +0530

    Updated Analytics descriptiom

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -2,6 +2,7 @@ const Analytics = () => {
   return (
     <div className="analytics">
       <h2>Analytics</h2>
+      <p>Track your progress and insights here.</p>
       {/* Analytics functionality goes here */}
     </div>
   );
